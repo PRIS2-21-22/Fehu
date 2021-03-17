cd769a238408605ddc22a185b6990c5f3def77aa
aaronblanco <aaronba13@hotmail.com>
2021-03-17 12:14:41 +0100

    Clase punto.

---
+++ point.py
@@ -0,0 +1,20 @@
+
+def point(self, x, y):
+    self._x = x
+    self._y = y
+
+
+def getPosX(self):
+    return self._x
+
+def setPosX(self, x):
+    self._x = x
+
+
+def getPosY(self):
+    return self._y
+
+def setPosY(self, y):
+    self._y = y
+def toString(self):
+    return "(" + getPosX() + ", " + getPosY() + ")"
