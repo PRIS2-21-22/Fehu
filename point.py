8d1ee401197ac95195bef38a1c7212bd27c7930c
aaronblanco <aaronba13@hotmail.com>
2021-03-17 12:18:40 +0100

    Cambios en la clase.

--- point.py
+++ point.py
@@ -10,11 +10,11 @@ def getPosX(self):
 def setPosX(self, x):
     self._x = x
 
-
 def getPosY(self):
     return self._y
 
 def setPosY(self, y):
     self._y = y
+    
 def toString(self):
-    return "(" + getPosX() + ", " + getPosY() + ")"
+    return "(" + self.getPosX() + ", " + self.getPosY() + ")"
